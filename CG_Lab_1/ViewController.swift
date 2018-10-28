0e8fd45b499c1624cdca7e1d1c99abc907d51b0f
Ivan <anonumus80@gmail.com>
2018-10-29 00:34:44 +0300

    Initial Commit

---
+++ CG_Lab_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CG_Lab_1
+//
+//  Created by Ivan Ermak on 10/29/18.
+//  Copyright © 2018 Ivan Ermak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CG_Lab_1Tests/CG_Lab_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  CG_Lab_1Tests.swift
+//  CG_Lab_1Tests
+//
+//  Created by Ivan Ermak on 10/29/18.
+//  Copyright © 2018 Ivan Ermak. All rights reserved.
+//
+
+import XCTest
+@testable import CG_Lab_1
+
+class CG_Lab_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
