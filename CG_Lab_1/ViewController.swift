a14d8384f80195b2b06aa2f233b805a37a96f6a4
Ivan <anonumus80@gmail.com>
2018-11-26 14:19:59 +0300

    v0.2
    Scale is ready

--- CG_Lab_1/ViewController.swift
+++ CG_Lab_1/ViewController.swift
@@ -28,14 +28,18 @@ class ViewController: UIViewController {
     }
     
     @IBAction func userStartsResize(_ sender: Any) {
-       
+      
         let data : CGDataProvider = CGDataProvider.init(filename: "/Users/buiver/Desktop/stw.png")!
         let color : CGColorRenderingIntent = CGColorRenderingIntent.defaultIntent
         let CGcoreTest : CGImage = CGImage(pngDataProviderSource: data, decode: nil, shouldInterpolate: false, intent: color)!
        debugPrint(CGcoreTest.width)
-        let CoreImage1 = image(fromPixelValues: CoreImage.image?.pixelData(), width: CGcoreTest.width, height: CGcoreTest.height)
+        let coefX = 0.7
+        let coefY = 0.7
+        let width1 = Int(Double(CGcoreTest.width) * coefX)
+        let height1 = Int(Double(CGcoreTest.height) * coefY)
+        let CoreImage1 = image(fromPixelValues: pixelScale(fromPixelValues: CoreImage.image?.pixelData(), width: CGcoreTest.width, height: CGcoreTest.height, scaleByY: coefY, scaleByX: coefX), width: width1, height: height1)
         var imageView : UIImageView
-      
+       
         imageView  = UIImageView(frame: CGRect(x: 20, y: 20, width: CoreImage1!.width, height: CoreImage1!.height))
         debugPrint(CoreImage1!.width)
         CoreImage.isHidden = true
@@ -83,7 +87,50 @@ class ViewController: UIViewController {
         
         return imageRef
     }
-    
+    func pixelScale(fromPixelValues pixelValues: [UInt16]?, width: Int, height: Int, scaleByY: Double, scaleByX: Double) -> [UInt16]? {
+        
+        var color: [UInt16] = []
+        var image: [UInt16] = []
+        var imageMatrix : [[[UInt16]]] = []
+        var resultMatrix : [[[UInt16]]] = []
+        var index = 0
+        var rows : [[UInt16]] = []
+        let newWidth = Int(Double(width) * scaleByX)
+        let newHeight = Int(Double(height) * scaleByY)
+        for _ in 0...height-1
+        {
+            
+            rows = []
+            for _ in 0...width-1
+            {
+                color = []
+                for _ in 0...3{
+               
+                color.append(pixelValues![index])
+                index += 1
+                }
+             /*   color = []
+                color?.append(pixelValues![4 * Int(Double(index)/scaleByX)])
+                color?.append(pixelValues![4 * Int(Double(index)/scaleByX) + 1])
+                color?.append(pixelValues![4 * Int(Double(index)/scaleByX) + 2])
+                color?.append(pixelValues![4 * Int(Double(index)/scaleByX) + 3]) */
+                rows.append(color)
+            }
+            imageMatrix.append(rows)
+        }
+        for y in 0...newHeight-1
+        {
+            for x  in 0...newWidth-1
+            {
+                let x1 = Int(Double(x)/scaleByX)
+                let y1 = Int(Double(y)/scaleByY)
+                image.append(contentsOf: (imageMatrix[y1][x1]))
+                
+            }
+        }
+        debugPrint(resultMatrix.count)
+        return image
+    }
 }
 extension UIImage {
     func pixelData() -> [UInt16]? {
@@ -103,6 +150,7 @@ extension UIImage {
         
         return pixelData
     }
+   
 }
 
 
